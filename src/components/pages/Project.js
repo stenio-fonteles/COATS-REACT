13226d814f0d69a91fc5fcf50405587f97b84fbc
stenio <macacovelho.ss@gmail.com>
2022-04-04 23:58:37 -0300

    término do get ao bd

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,10 +1,30 @@
 import { useLocation } from "react-router-dom";
-
+import { useState,useEffect } from "react";
 import Message from "../Layout/messege/messege";
 import LinkProjects from '../Layout/linkProjects/linkprojects'
 import styles from './projects.module.css'
+import ProjectCard from '../Project/projectCard'
 function Project(){
+
+    //GET DO BD
+    const [projects, setProjects] = useState([])
     
+    useEffect(()=>{
+        fetch('http://localhost:5000/project',{
+            method: 'GET',
+            Headers:{
+                'Content-Type' : 'application/json',
+            },
+
+        })
+        .then((resp) => resp.json())
+        .then((data)=> {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    },[])
+
+    //.....//.....
     const localizacao = useLocation()
     let message = "";
     if(localizacao.state){
@@ -18,8 +38,17 @@ function Project(){
                 <LinkProjects to="/NewProject/" text="Criar Projeto"/>
             </div>
                 {message && <Message type="success" msg={message}/>}
-            <div>
-                <h3>Projetos...</h3>
+            <div className="styles.teste">
+                {projects.length > 0 && 
+                projects.map((project) => (
+                    <ProjectCard 
+                    name = {project.name}
+                    id = {project.id}
+                    budget={project.budget}
+                    category={project.category.name}
+                    kay={project.id}
+                    />
+                ))}
 
             </div>
                 
